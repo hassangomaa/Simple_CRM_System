de175b0b74fcbde1c860ee5d0d02564a4557aa14
Hassan Gomaa <hassangomaa974@gmail.com>
2023-05-12 06:16:06 +0300

    routes updated with no duplicates

--- routes/web.php
+++ routes/web.php
@@ -7,21 +7,14 @@ use Illuminate\Support\Facades\Route;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
+| Here is where you can register web routes for your application.
+| These routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::redirect('/', '/home');
 
-
-//Route::redirect('/', '/home');
-//
-//Auth::routes();
-//
 Route::middleware('auth')->group(function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
@@ -31,30 +24,4 @@ Route::middleware('auth')->group(function () {
     Route::resource('tasks', \App\Http\Controllers\TasksController::class);
 
     Route::get('/admin/dashboard', [\App\Http\Controllers\AdminController::class, 'dashboard'])->name('dashboard');
-
-
 });
-
-//Auth::routes();
-
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-//Auth::routes();
-
-//Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
-
-//Route::group(['middleware' => 'auth'], function () {
-//	Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
-//	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
-//	Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
-//	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
-//	Route::get('{page}', ['as' => 'page.index', 'uses' => 'App\Http\Controllers\PageController@index']);
-//});
-
-
-Auth::routes();
-
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-//Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
